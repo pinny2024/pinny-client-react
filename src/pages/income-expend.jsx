b4ea57577b1a3d79aa194918b302fc541e77ac83
seonyo <jsy6535@naver.com>
2024-03-28 02:57:44 +0900

    Feat: data-none 컴포넌트 생성

--- src/pages/income-expend.jsx
+++ src/pages/income-expend.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ListAttributes from "../componets/income-expend/list-attributes"
 import PostBtn from "../componets/income-expend/post-btn";
+import DataNone from "../componets/income-expend/data-none";
 import Nav  from "../componets/comm/nav";
 
 const IncomeExpend = () => {
@@ -8,6 +9,7 @@ const IncomeExpend = () => {
         <div>
             <ListAttributes/>
             <PostBtn/>
+            <DataNone/>
             <Nav/>
         </div>
     )
