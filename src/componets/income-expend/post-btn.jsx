f026022f49040d7fa449c5de57b58c9e6f02cbcf
seonyo <jsy6535@naver.com>
2024-03-28 02:43:45 +0900

    Feat: 수입/지출 글쓰기 버튼 컴포넌트화

---
+++ src/componets/income-expend/post-btn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import "../../css/income-expend/post-btn.css";
+
+
+const PostBtn = () => {
+    return (
+        <div className="post-btn">
+            <Icon className="pencil-icon" icon="mingcute:pencil-fill"/>
+        </div>
+    )
+}
+
+export default PostBtn;
--- src/pages/income-expend.jsx
+++ src/pages/income-expend.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ListAttributes from "../componets/income-expend/list-attributes"
+import PostBtn from "../componets/income-expend/post-btn";
 import Nav  from "../componets/comm/nav";
 
 const IncomeExpend = () => {
     return(
         <div>
             <ListAttributes/>
+            <PostBtn/>
             <Nav/>
         </div>
     )
