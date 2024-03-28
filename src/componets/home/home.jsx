58ee3968da78577d7c8424f2a54b0031f51717d1
GoEun <w2219@e-mirim.hs.kr>
2024-03-29 02:44:59 +0900

    Feat: 파일나누기

--- src/componets/home/home.jsx
+++ src/componets/home/home.jsx
@@ -3,42 +3,22 @@ import "../../css/comm/index.css"
 import "../../css/home/home.css"
 import TopSheet from "./TopSheet";
 import "../../pages/income-expend"
+import WeekPlan from "../home/week-plan"
 import Nav from "../comm/nav";
 
 const Home = () => {
     return (
         <div className="plan">
-            <div className="top"> 
+            <div className="top-calender"> 
                <TopSheet />
-                <div className="use-money">
-                    
-                </div> 
-                <div className="week-plan">
-                    <div className="week-plan-name">
-                        일주일 예산을<br />세워보세요!
-                    </div>
-                    <div className="week-plan-button">
-                        <button>예산 세우기</button>
-                    </div>
-                </div>
             </div>
-            <hr />
-            <div className="career">
-                <div className="career-name">
-                    오늘 실천한 계획을 체크해 주세요!
-                </div>
-                <div className="career-button">
-                    <button></button>
-                    <button></button>
-                    <button></button>
-                    <button></button>
-                    <button></button>
-                </div>
+            <div className="center-plan">
+               <WeekPlan />
             </div>
             <div className="bottom-bar">
                 <Nav />
             </div>
-
+            
         </div>
     )
 }
