742f3d911946669abf78bdb655bff07a52e65a82
seonyo <jsy6535@naver.com>
2024-03-29 03:12:00 +0900

    Feat: header 컴포넌트 생성

---
+++ src/componets/comm/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
+
+import "../../css/comm/index.css"
+import "../../css/comm/header.css"
+
+const Header = (props) => {
+    const {text} = props;
+    const navigate = useNavigate();
+
+    return(
+        <div className="header-container">
+            <Icon className="header-icon" icon="material-symbols-light:arrow-back-ios-rounded" onClick={()=>navigate(-1)}/>
+            <div className="header-text">{text}</div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/income-expend-post.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../componets/comm/header";
+
+const IncomeExpendPost = () => {
+    return(
+        <div>
+          <Header text="계획 세우기!"/>
+        </div>
+    )
+}
+
+export default IncomeExpendPost;
