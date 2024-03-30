81546451d123adb8a9c477af2114c44cd05be863
seonyo <jsy6535@naver.com>
2024-03-31 02:27:56 +0900

    Feat: imcome-expend select-btn 컴포넌트 생성

--- src/pages/income-expend-post.jsx
+++ src/pages/income-expend-post.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../componets/comm/header";
+import SelectBtn from "../componets/income-expend/select-btn";
 
 const IncomeExpendPost = () => {
     return(
         <div>
           <Header text="계획 세우기!"/>
+          <SelectBtn/>
         </div>
     )
 }
