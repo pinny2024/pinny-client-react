4029ce9c0fc410ebff14d7200fe9b78b92cd20ea
seonyo <jsy6535@naver.com>
2024-04-02 00:00:45 +0900

    Feat: 수입 기록 페이지 InputData 컴포넌트 추가

--- src/pages/income-post.jsx
+++ src/pages/income-post.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../componets/comm/header";
 import SelectBtn from "../componets/income-expend/select-btn";
+import InputData from "../componets/income-expend/input-data";
 
 const ExpendPost = () => {
     return(
         <div>
           <Header text="계획 세우기!"/>
           <SelectBtn/>
+          <InputData/>
         </div>
     )
 }
