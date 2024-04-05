9a341e193e2bb2ccdf9eb0a137b7b2d7cd1a05c8
seonyo <jsy6535@naver.com>
2024-04-05 18:08:08 +0900

    Feat: input-data select box에 drop down 연결

--- src/componets/income-expend/input-data.jsx
+++ src/componets/income-expend/input-data.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "../../componets/income-expend/dropdown"
+import { Icon } from "@iconify/react";
 
 import "../../css/comm/index.css"
 import "../../css/income-expend/input-data.css"
 
 const InputData = () => {
+    const [isExpand, setIsExpand] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("식비");
+    const [selectedImageURL, setSelectedImageURL] = useState("food-gray-icon.svg")
+
+    const articleBtnExpandHandler = () => {
+        setIsExpand(!isExpand);
+    };
+
+    const handleCategorySelection = (category, imageURL) => {
+        setSelectedCategory(category);
+        setSelectedImageURL(imageURL);
+        setIsExpand(false);
+    };
+
     return (
         <>
             <div className="input-data-container">
@@ -13,7 +29,16 @@ const InputData = () => {
                 </div>
                 <div className="input-data-box">
                     <p className="input-data-text">분류</p>
-                    <input type="text" className="input-data-textbox" />
+                    {!isExpand && (
+                        <button className="input-data-selectbox" onClick={articleBtnExpandHandler}>
+                            <img src={`${process.env.PUBLIC_URL}/img/income-expend/${selectedImageURL}`} />
+                            <p>{selectedCategory}</p>
+                            <Icon icon="uiw:down" className="input-data-icon"></Icon>
+                        </button>
+                    )}
+                    {isExpand && (
+                        <Dropdown handleCategorySelection={handleCategorySelection} />
+                    )}
                 </div>
                 <div className="input-data-box">
                     <p className="input-data-text">내용</p>
