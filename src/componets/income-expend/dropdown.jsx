868c7c5c3e8dbc437cc504d458e5a53e8a54d843
seonyo <jsy6535@naver.com>
2024-04-06 16:03:40 +0900

    Feat: Dropdown 스타일 변경

--- src/componets/income-expend/dropdown.jsx
+++ src/componets/income-expend/dropdown.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 
 import "../../css/comm/index.css"
@@ -6,15 +7,19 @@ import "../../css/income-expend/dropdown.css"
 const Dropdown = ({ handleCategorySelection }) => {
     const categories = ["식비", "교통수단", "문화생활", "금융", "기타", "저축"];
     const imgURL = ["food-gray-icon.svg", "car-gray-icon.svg", "music-gray-icon.svg", "money-gray-icon.svg", "etc-gray-icon.svg", "piggy-gray-icon.svg"];
+    const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
     return (
         <div className="dropdown-container">
             <ul>
                 {categories.map((category, index) => (
-                    <li key={index} onClick={() => handleCategorySelection(category, imgURL[index])}>
+                    <li key={index} onClick={() => {
+                        setSelectedCategory((prev) => selectedCategory);
+                        handleCategorySelection(categories[index], imgURL[index]);
+                    }}>
                         <img src={`${process.env.PUBLIC_URL}/img/income-expend/${imgURL[index]}`} />
-                        <p>{category}</p>
-                        {index === 0 && <Icon icon="uiw:down" className="input-data-icon"/>}
+                        <p style={{ color: selectedCategory === category ? "rgb(0,0,0,0.87)" : "rgb(0,0,0,0.34)" }}>{category}</p>
+                        {index === 0 && <Icon icon="uiw:sdown" className="input-data-icon" />}
                     </li>
                 ))}
             </ul>
