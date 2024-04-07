6bf0d66a095485f5e17b6239e902d28e7567800d
GoEun <w2219@e-mirim.hs.kr>
2024-04-07 18:06:31 +0900

    Feat: 1주차 data

---
+++ src/componets/plan/week/first-week-data.jsx
@@ -0,0 +1,14 @@
+// 예시: FirstWeek.js
+import React, { useState } from "react";
+
+const FirstWeek = () => {
+    const [data, setData] = useState(/* 해당 주차의 데이터 */);
+
+    return (
+        <div>
+            {/* 해당 주차의 데이터를 UI에 표시 */}
+        </div>
+    );
+};
+
+export default FirstWeek;
