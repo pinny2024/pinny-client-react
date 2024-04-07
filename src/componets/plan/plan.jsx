35ab7c0340fe82afb302bd35e23227635366cbb0
GoEun <w2219@e-mirim.hs.kr>
2024-04-07 18:08:43 +0900

    Feat: 계획 컴포넌트 생성

--- src/componets/plan/plan.jsx
+++ src/componets/plan/plan.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Nav from "../comm/nav";
+import PostBtn from "./post-btn";
+import TopWeekStatus from "./week/top-week-status";
+import JobPlan from "./job/job-plan";
 
 const Plan = () => {
     return (
         <div className="plan-status">
             <div className="top-week-status"> 
-               
+               <TopWeekStatus />
             </div>
             <div className="job-plan">
-               
+                <JobPlan />
+            </div>
+            <div className="post-btn">
+                <PostBtn />
             </div>
             <div className="bottom-bar">
                 <Nav />
