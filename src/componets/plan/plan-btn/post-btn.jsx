fb53cfbe1dd0caefc1568b87e337d246ec1c6f53
GoEun <w2219@e-mirim.hs.kr>
2024-04-13 17:55:29 +0900

    Feat: plan category 생성,

---
+++ src/componets/plan/plan-btn/post-btn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
+
+import "../../../css/income-expend/post-btn.css";
+
+const PostBtn = () => {
+    return (
+        <Link to="/plan/plan-categroy">
+            <div className="post-btn">
+                <Icon className="pencil-icon" icon="mingcute:pencil-fill" />
+            </div>
+        </Link>
+    )
+}
+
+export default PostBtn;
---
+++ src/pages/plan/plan-category.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlanCategory from "../../componets/plan/plan-btn/plan-category";
+import Header from "../../componets/comm/header";
+const PlanCategoryPage = () => {
+    return(
+        <div>
+            <Header text="계획 세우기!"/>
+            <PlanCategory />
+        </div>
+    )
+}
+
+export default PlanCategoryPage;
