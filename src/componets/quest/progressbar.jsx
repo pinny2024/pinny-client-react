612e280f745fa76ae58337bbca6ee3c3ffea4296
seonyo <jsy6535@naver.com>
2024-04-14 02:28:32 +0900

    Feat: progressbar 컴포넌트 생성

---
+++ src/componets/quest/progressbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../css/comm/index.css"
+import "../../css/quest/progressbar.css"
+
+const Progressbar = () => {
+    return(
+        <div className="progressbar-container">
+            <div className="progressbar-background">
+            </div>
+        </div>
+    );
+};
+
+export default Progressbar;
