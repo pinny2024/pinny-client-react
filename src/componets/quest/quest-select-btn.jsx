0e22f77dff42fe446608c8c6a00200c981496c89
seonyo <jsy6535@naver.com>
2024-04-25 01:39:34 +0900

    Refactor: quest-select 모든 버튼을 컴포넌트로 분리

--- src/componets/quest/quest-select-btn.jsx
+++ src/componets/quest/quest-select-btn.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import { React, useState } from "react";
 
 import "../../css/comm/index.css"
 import "../../css/quest/quest-select-btn.css";
+import { type } from "@testing-library/user-event/dist/type";
 
-const QuestSelectBtn = (props) => {
-    const { img, text } = props;
+const QuestSelectBtn = () => {
+    const items = [
+        { type: '가방', img: 'bag-icon.svg', },
+        { type: '옷', img: 'clothes-icon.svg', },
+        { type: '악세사리', img: 'ring-icon.svg', },
+        { type: '전자기기', img: 'laptop-icon.svg', },
+        { type: '여행', img: 'airplane-icon.svg', },
+        { type: '집', img: 'house-icon.svg', },
+        { type: '차', img: 'car-icon.svg', },
+        { type: '문화생활', img: 'ticket-icon.svg', },
+        { type: '기타', img: 'etc-icon.svg', }
+    ];
+
+    const [select, setSelect] = useState('');
+
+    const handleClick = (type) => {
+        setSelect(type);
+    };
+
     return (
         <div className="quest-btn-container">
-            <div className="quest-btn-box">
-                <img src={`${process.env.PUBLIC_URL}/img/quest/${img}`} className="quest-btn-icon" />
-            </div>
-            <div className="quest-btn-text">{text}</div>
+            {items.map((item, index) => (
+                <div
+                    key={index}
+                    onClick={() => handleClick(item.type)}
+                    className={`${select === item.type ? 'select' : 'quest-btn-box'}`}
+                >
+                    <img src={`${process.env.PUBLIC_URL}/img/quest/${item.img}`} className="quest-btn-icon" />
+                </div>
+            ))}
+
+            {/* <div className="quest-btn-text">{text}</div> */}
         </div>
     )
 }
--- src/componets/quest/quest-select.jsx
+++ src/componets/quest/quest-select.jsx
@@ -1,23 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestSelectBtn from "./quest-select-btn";
 
 import "../../css/comm/index.css"
 import "../../css/quest/quest-select.css";
 const QuestSelect = () => {
+    const [selectedBtn, setSelectedBtn] = useState(null);
+
+    const handleButtonClick = (btnText) => {
+        setSelectedBtn(btnText);
+    };
+    
     return (
         <>
             <div className="quest-select-title">아이콘을 선택하세요</div>
-            <div className="quest-select-btn">
-                <QuestSelectBtn img="bag-icon.svg" text="가방" />
-                <QuestSelectBtn img="clothes-icon.svg" text="옷" />
-                <QuestSelectBtn img="ring-icon.svg" text="악세사리" />
-                <QuestSelectBtn img="laptop-icon.svg" text="전자기기" />
-                <QuestSelectBtn img="airplane-icon.svg" text="여행" />
-                <QuestSelectBtn img="house-icon.svg" text="집" />
-                <QuestSelectBtn img="car-icon.svg" text="차" />
-                <QuestSelectBtn img="ticket-icon.svg" text="문화생활" />
-                <QuestSelectBtn img="etc-icon.svg" text="기타" />
-            </div>
+            <QuestSelectBtn/>
             <div className="quest-select-next">다음</div>
         </>
     )
