a71028a8c6cdd48489b1124b9d8d5d2f89279e15
seonyo <jsy6535@naver.com>
2024-04-26 01:56:06 +0900

    Feat: 경로에 따른 데이터  input-data  넘기기

--- src/componets/quest/input-data.jsx
+++ src/componets/quest/input-data.jsx
@@ -6,16 +6,16 @@ import styles from "../../css/quest/input-data.module.css"
 const InputData = (props) => {
     return (
         <div className={styles['input-container']}>
-            <div className={styles['title']}>내용을 입력해주세요</div>
+            <div className={styles['title']}>{props.title}</div>
             <div className={styles['input-box']}>
-                <div className={styles['input-title']}>목표</div>
+                <div className={styles['input-title']}>{props.inputTitle}</div>
                 <input 
                 type="text"
                 className={styles['input']}
                 placeholder="헨드백 사기"
                 />
             </div>
-            <div className={styles['input-btn']}>다음</div>
+            <div className={styles['input-btn']}>{props.btn}</div>
         </div>
     )
 }
--- src/pages/quest/quest-input.jsx
+++ src/pages/quest/quest-input.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../../componets/comm/header";
 import InputData from "../../componets/quest/input-data";
 
 const QuestInput = () => {
+    const [path, setPath] = useState('');
     const location = useLocation();
-    console.log(location);
-    return(
+    useEffect(() => {
+        if (location.pathname.endsWith("content"))
+            setPath("content")
+        else if (location.pathname.endsWith("price"))
+            setPath("price");
+    }, [location.pathname]);
+    return (
         <>
-            <Header text = "목표 만들기"/>
-            <InputData/>
+            <Header text="목표 만들기" />
+            {path === "content" && <InputData title="내용을 입력해주세요" inputTitle="목표" btn="다음" />}
+            {path === "price" && <InputData title="가격을 입력해주세요" inputTitle="가격" btn="목표 만들기" />}
         </>
     )
 }
