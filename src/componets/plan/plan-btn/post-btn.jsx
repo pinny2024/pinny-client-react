81876a8398ffbb391ae79d6f1ffcbf87d7e227c9
GoEun <w2219@e-mirim.hs.kr>
2024-05-13 00:22:25 +0900

    Feat: css 주소 수정

--- src/componets/plan/plan-btn/post-btn.jsx
+++ src/componets/plan/plan-btn/post-btn.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 
-import "../../../css/income-expend/post-btn.module.css";
+import styles from "../../../css/income-expend/post-btn.module.css";
 
 const PostBtn = () => {
     return (
         <Link to="/plan/plan-categroy">
-            <div className="post-btn">
-                <Icon className="pencil-icon" icon="mingcute:pencil-fill" />
+            <div className={styles['post-btn']}>
+                <Icon className={styles['pencil-icon']} icon="mingcute:pencil-fill" />
             </div>
         </Link>
     )
