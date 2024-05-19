7f86584770898fffd2b6237d034ef2064b5bfb94
seonyo <jsy6535@naver.com>
2024-05-20 02:16:16 +0900

    Feat: inputbox에 값이 있으면 확인 버튼 색상 변경

--- src/componets/quest/input-data.jsx
+++ src/componets/quest/input-data.jsx
@@ -9,20 +9,32 @@ const InputData = (props) => {
     const category = location.state.category;
 
     const [content, setContent] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
     useEffect(() => {
         switch (category) {
             case '가방': setContent('핸드백 사기'); break;
-            case '옷' : setContent('AA브랜드 옷 사기'); break;
-            case '악세사리' : setContent('목걸이 사기'); break;
-            case '전자기기' : setContent('노트북 사기'); break;
-            case '집' : setContent('원룸 얻기'); break;
-            case '여행' : setContent('미국 가기'); break;
-            case '차' : setContent('자가용 뽑기'); break;
-            case '문화생활' : setContent('콘서트 티켓 가기'); break;
-            case '기타' : setContent('기부하기'); break;
+            case '옷': setContent('AA브랜드 옷 사기'); break;
+            case '악세사리': setContent('목걸이 사기'); break;
+            case '전자기기': setContent('노트북 사기'); break;
+            case '집': setContent('원룸 얻기'); break;
+            case '여행': setContent('미국 가기'); break;
+            case '차': setContent('자가용 뽑기'); break;
+            case '문화생활': setContent('콘서트 티켓 가기'); break;
+            case '기타': setContent('기부하기'); break;
         }
     }, [category]);
+
+    useEffect(() => {
+        const inputBox = document.querySelector(`.${styles['input-btn']}`);
+        if (inputValue) {
+            inputBox.style.backgroundColor = '#5EDCC4';
+        }
+    }, [inputValue]);
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
 
     return (
         <div className={styles['input-container']}>
@@ -33,9 +45,10 @@ const InputData = (props) => {
                     type="text"
                     className={styles['input']}
                     placeholder={`${content}`}
+                    onChange={handleInputChange}
                 />
             </div>
-            <div className={styles['input-btn']}>   {props.btn}</div>
+            <div className={styles['input-btn']}>  {props.btn}</div>
         </div>
     )
 }
