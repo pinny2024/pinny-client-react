e2bb83424cce39b61a86649f4536da608279ae1d
seonyo <jsy6535@naver.com>
2024-05-20 09:52:04 +0900

    Feat: 이전목표 슬라이드 구현

--- src/pages/quest/quest.jsx
+++ src/pages/quest/quest.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import BackgroundQuest from "../../componets/quest/background";
 import Progressbar from "../../componets/quest/progressbar";
+import PreQuest from "../../componets/quest/pre-quest";
+import Nav from "../../componets/comm/nav";
 const QuestData = () => {
     return (
         <>
             <BackgroundQuest />
             <Progressbar/>
+            <PreQuest/>
+            <Nav/>
         </>
     )
 }
