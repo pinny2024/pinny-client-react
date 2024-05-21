0f2dcb5e18892b42d5acb863d747a1f5ebeed113
GoEun <w2219@e-mirim.hs.kr>
2024-05-22 08:24:27 +0900

    계획 세우기 컴포넌트

---
+++ src/pages/plan/plan-detail.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../componets/comm/header";
+import PlanDetail from "../../componets/plan/plan-btn/plan-detail";
+const PlanDetailPage = () => {
+    return(
+        <div>
+            <Header text="계획 세우기!"/>
+            <PlanDetail />
+        </div>
+    )
+}
+
+export default PlanDetailPage;
