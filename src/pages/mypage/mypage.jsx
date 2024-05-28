63d2223b4ec47a2a939ae41779967b43a8b5fd29
seonyo <jsy6535@naver.com>
2024-05-28 09:51:40 +0900

    Feat: mypage 구현

--- src/pages/mypage/mypage.jsx
+++ src/pages/mypage/mypage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Profile from "../../componets/mypage/profile"
 import Badge from "../../componets/mypage/badge"
+import Overconsumption from "../../componets/mypage/overconsumption";
+import Nav from "../../componets/comm/nav";
 
 const MypagePage = () => {
     return (
         <>
             <Profile />
             <Badge />
+            <Overconsumption/>
+            <Nav />
         </>
     )
 }
