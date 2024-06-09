99f77a4a0bb5962ffe81a51e91c94f85368ee77e
seonyo <jsy6535@naver.com>
2024-06-10 04:18:29 +0900

    Fix: inputbox가 다 채워졌을 때 버튼 색상 변경

--- src/componets/income-expend/post/input-data.jsx
+++ src/componets/income-expend/post/input-data.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "./dropdown"
 import { Icon } from "@iconify/react";
 
@@ -8,7 +8,9 @@ import styles from "../../../css/income-expend/input-data.module.css"
 const InputData = () => {
     const [isExpand, setIsExpand] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState("식비");
-    const [selectedImageURL, setSelectedImageURL] = useState("food-gray-icon.svg")
+    const [selectedImageURL, setSelectedImageURL] = useState("food-gray-icon.svg");
+    const [amount, setAmount] = useState("");
+    const [content, setContent] = useState("");
 
     const articleBtnExpandHandler = () => {
         setIsExpand(!isExpand);
@@ -20,12 +22,19 @@ const InputData = () => {
         setIsExpand(false);
     };
 
+    const isFormComplete = amount !== "" && selectedCategory !== "" && content !== "";
+
+
     return (
         <>
             <div className={styles['container']}>
                 <div className={styles['box']}>
                     <p className={styles['text']}>금액</p>
-                    <input type="text" className={styles['textbox']}/>
+                    <input
+                        type="text"
+                        className={styles['textbox']}
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)} />
                 </div>
                 <div className={styles['box']}>
                     <p className={styles['text']}>분류</p>
@@ -42,11 +51,18 @@ const InputData = () => {
                 </div>
                 <div className={styles['box']}>
                     <p className={styles['text']}>내용</p>
-                    <input type="text" className={styles['textbox']}/>
+                    <input type="text"
+                        className={styles['textbox']}
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)} />
                 </div>
             </div>
             <div className={styles['done']}>
-                <button className={styles['done-btn']}>확인</button>
+                <button
+                    className={`${styles['done-btn']} ${isFormComplete ? styles['done-btn-active'] : ''}`}
+                >
+                    확인
+                </button>
             </div>
         </>
     )
