6560891fe667cd1829bcf9d59b14011b62df84f4
seonyo <jsy6535@naver.com>
2024-06-10 02:27:46 +0900

    Feat: 로그인 api 연동

--- src/componets/auth/login.jsx
+++ src/componets/auth/login.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../../css/auth/login.css"
 import "../../css/comm/index.css"
+import config from '../../config';
+
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
 
-    const handleTermUse = () =>{
+    const handleTermUse = () => {
         navigate('/term-use')
+    }
+
+    const handleSubmitClick = () => {
+        const data = {
+            email: email,
+            password: password
+        };
+
+        axios.post(`${config.baseUrl}/login`, data)
+            .then(function (response) {
+                console.log(response);
+                alert("로그인 성공");
+
+                const id = response.data.userId;
+                localStorage.setItem("id", id);
+                navigate("/home");
+            })
+            .catch(function (error) {
+                alert("로그인 실패");
+                console.log(error);
+            })
     }
 
     return (
@@ -17,9 +43,15 @@ const Login = () => {
                 <h1>돈 모으기를 시작해볼까요?</h1>
             </div>
             <div className="login-main">
-                <input type="text" className="login-input-email" placeholder="이메일을 입력하세요"></input>
-                <input type="text" className="login-input-password" placeholder="비밀번호를 입력해주세요"></input>
-                <button type="submit" className="login-submit-btn">확인</button>
+                <input type="text"
+                    className="login-input-email"
+                    placeholder="이메일을 입력하세요"
+                    onChange={(e) => setEmail(e.target.value)}></input>
+                <input type="text"
+                    className="login-input-password"
+                    placeholder="비밀번호를 입력해주세요"
+                    onChange={(e) => setPassword(e.target.value)}></input>
+                <button type="submit" className="login-submit-btn" onClick={handleSubmitClick}>확인</button>
                 <div className="login-list">
                     <p onClick={handleTermUse}>회원가입</p>
                     <p>아이디/비번 찾기</p>
