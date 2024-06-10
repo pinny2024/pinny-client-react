94f1a7518463fe6d092f419eadcc3c865d112e92
GoEun <w2219@e-mirim.hs.kr>
2024-06-10 14:26:43 +0900

    계획 입력 추가 서버연동

--- src/componets/plan/plan-btn/plan-detail.jsx
+++ src/componets/plan/plan-btn/plan-detail.jsx
@@ -1,40 +1,67 @@
-// PlanDetail.jsx
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../../../css/plan/plan-btn/plan-detail.css';
+import axios from '../../utils/axiosInstance';
 
 const PlanDetail = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { selectedCategories, category, image } = location.state;
 
-    const [inputValue, setInputValue] = useState(""); 
+  const [inputValue, setInputValue] = useState('');
+  const categoryNames = selectedCategories.map(cat => cat.name);
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value); 
-    };
+  useEffect(() => {
+    console.log('Selected categories:', selectedCategories);
+    console.log('Image:', image); // 이미지 값 콘솔에 출력
+  }, [selectedCategories, categoryNames, image]);
 
-    const handleNextButtonClick = () => {
-        navigate('/plan/plan-list', { state: { inputValue } });
-    };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    return (
-        <div>
-            <div className="top-detail">
-                내용을 입력해주세요
-            </div>
-            <div className="detail-plan-box">
-                계획
-                <input
-                    type="text" 
-                    placeholder="편의점에서 3000원 이상 사지 않기"
-                    value={inputValue} 
-                    onChange={handleInputChange} 
-                />
-            </div>
-            <div className="plan-detail-check">
-                <button className={inputValue ? "mint-button" : "grey-button"} onClick={handleNextButtonClick}>확인</button>
-            </div>
-        </div>
-    );
+  const handleNextButtonClick = async () => {
+    try {
+      if (inputValue.trim() !== '') {
+        await axios.post('/plans', {
+          categories: selectedCategories,
+          plan: inputValue,
+        });
+        navigate('/plan/plan-list', {
+          state: {
+            categoryNames,
+            plan: inputValue, 
+            image, 
+          },
+        });
+      }
+    } catch (error) {
+      console.error('Error adding plan detail:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="top-detail">내용을 입력해주세요</div>
+      <div className="detail-plan-box">
+        계획
+        <input
+          type="text"
+          placeholder="편의점에서 3000원 이상 사지 않기"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="plan-detail-check">
+        <button
+          className={inputValue.trim() !== '' ? 'mint-button' : 'grey-button'}
+          onClick={handleNextButtonClick}
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PlanDetail;
