8bc8e5a601c7f6d7a5e02109e7d96f6a09807088
GoEun <w2219@e-mirim.hs.kr>
2024-06-10 14:27:51 +0900

    카테고리 이미지 연동

--- src/componets/plan/plan-btn/top-plan-category.jsx
+++ src/componets/plan/plan-btn/top-plan-category.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import '../../../css/plan/plan-btn/top-plan-category.css';
+import categories from './categories';
 
 const TopPlanCategory = ({ handleButtonClick, clickedButtons }) => {
     return (
         <div className="top-plan-category">
             <div className="plan-category-button">
-                <button className={clickedButtons.food ? "category-button clicked" : "category-button"} onClick={() => handleButtonClick('food')}>
-                    <img className="button-image" src={`${process.env.PUBLIC_URL}/img/job-plan/category-button/category-food-white.svg`} alt="음식" />
-                </button>
-                <button className={clickedButtons.car ? "category-button clicked" : "category-button"} onClick={() => handleButtonClick('car')}>
-                    <img className="button-image" src={`${process.env.PUBLIC_URL}/img/job-plan/category-button/category-car-white.svg`} alt="차" />
-                </button>
-                <button className={clickedButtons.music ? "category-button clicked" : "category-button"} onClick={() => handleButtonClick('music')}>
-                    <img className="button-image" src={`${process.env.PUBLIC_URL}/img/job-plan/category-button/category-music-white.svg`} alt="음악" />
-                </button>
-                <button className={clickedButtons.money ? "category-button clicked" : "category-button"} onClick={() => handleButtonClick('money')}>
-                    <img className="button-image" src={`${process.env.PUBLIC_URL}/img/job-plan/category-button/category-money-white.svg`} alt="돈" />
-                </button>
-                <button className={clickedButtons.etc ? "category-button clicked" : "category-button"} onClick={() => handleButtonClick('etc')}>
-                    <img className="button-image" src={`${process.env.PUBLIC_URL}/img/job-plan/category-button/category-etc-white.svg`} alt="기타" />
-                </button>
+                {categories.map(category => (
+                    <button
+                        key={category.name}
+                        className={`category-button ${clickedButtons[category.name] ? 'clicked' : ''}`}
+                        onClick={() => handleButtonClick(category.name, category.image)}
+                    >
+                        <img
+                            className="button-image"
+                            src={`${process.env.PUBLIC_URL}${category.image}`}
+                            alt={category.alt}
+                        />
+                    </button>
+                ))}
             </div>
         </div>
     );
