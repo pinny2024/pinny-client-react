b17d2b4a2e8c0aac983ee08c6b67027c78bd4519
GoEun <w2219@e-mirim.hs.kr>
2024-06-10 14:27:21 +0900

    계획 입력 수정 서버연동

---
+++ src/componets/plan/plan-btn/categories.jsx
@@ -0,0 +1,9 @@
+const categories = [
+    { name: 'food', image: '/img/job-plan/category-button/category-food-white.svg', alt: '음식' },
+    { name: 'car', image: '/img/job-plan/category-button/category-car-white.svg', alt: '차' },
+    { name: 'music', image: '/img/job-plan/category-button/category-music-white.svg', alt: '음악' },
+    { name: 'money', image: '/img/job-plan/category-button/category-money-white.svg', alt: '돈' },
+    { name: 'etc', image: '/img/job-plan/category-button/category-etc-white.svg', alt: '기타' },
+];
+
+export default categories;
