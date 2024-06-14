e1802b6de5dd14ca61b837531fb1b18e61a39ac1
seonyo <jsy6535@naver.com>
2024-06-14 11:18:09 +0900

    Feat: 모아보기 api 연동

--- src/componets/income-expend/money-history/money-history.jsx
+++ src/componets/income-expend/money-history/money-history.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoneyHistoryAttribute from "./money-history-attribute";
+import axios from "axios";
+import config from "../../../config";
 
 import "../../../css/comm/index.css"
 import styles from "../../../css/income-expend/money-history.module.css"
 
 const MoneyHistory = () => {
+    const userId = localStorage.getItem("id");
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${config.baseUrl}/transactions/${userId}/type`)
+        .then((response) => {
+            console.log(response);
+            setTransactions(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }, []);
+
     return (
         <div className={styles['box']}>
             <div className={styles['day']}>9일</div>
-            <MoneyHistoryAttribute />
-            <MoneyHistoryAttribute />
-            <MoneyHistoryAttribute />
+            {transactions.map((transaction, index) => (
+                <MoneyHistoryAttribute
+                    key={index}
+                    name={transaction.name}
+                    description={transaction.description}
+                    type={transaction.type}
+                    category={transaction.category}
+                />
+            ))}
         </div>
     )
 }
