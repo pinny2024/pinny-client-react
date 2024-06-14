4c4073c2c2d848a56cbdaee6af66ec60a7137371
seonyo <jsy6535@naver.com>
2024-06-14 19:16:17 +0900

    Fix: slider 제거 및 css 수정

--- src/componets/quest/progressbar.jsx
+++ src/componets/quest/progressbar.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
 import "../../css/comm/index.css";
 import styles from "../../css/quest/progressbar.module.css";
 import axios from "axios";
@@ -24,45 +23,31 @@ const Progressbar = () => {
             })
     }, [userId]);
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
-
     return (
         <div className={styles['container']}>
-            <Slider {...settings}>
-                {questData.map((quest, index) => (
-                    <div key={index} className={styles['slider-item']}>
-                        <div className={styles['background']}>
-                            <CircularProgressbarWithChildren
-                                value={0} 
-                                strokeWidth={11}
-                                className={styles['main']}
-                                styles={{
-                                    root: { height: "32.25vh" },
-                                    path: {
-                                        stroke: "#79AFEF",
-                                        strokeLinecap: "round",
-                                        transition: "stroke-dashoffset 0.5s ease 0s",
-                                    },
-                                    trail: {
-                                        stroke: "rgb(0,0,0,0.1)"
-                                    }
-                                }}
-                            >
-                                <div className={styles['starter']}></div>
-                                <Link to="/quest/select">
-                                    <div className={styles['icon']}>?</div>
-                                </Link>
-                            </CircularProgressbarWithChildren>
-                        </div>
-                    </div>
-                ))}
-            </Slider>
+            <div className={styles['background']}>
+                <CircularProgressbarWithChildren
+                    value={0}
+                    strokeWidth={11}
+                    className={styles['main']}
+                    styles={{
+                        root: { height: "33vh" },
+                        path: {
+                            stroke: "#79AFEF",
+                            strokeLinecap: "round",
+                            transition: "stroke-dashoffset 0.5s ease 0s",
+                        },
+                        trail: {
+                            stroke: "rgb(0,0,0,0.1)"
+                        }
+                    }}
+                >
+                    <div className={styles['starter']}></div>
+                    <Link to="/quest/select">
+                        <div className={styles['icon']}>?</div>
+                    </Link>
+                </CircularProgressbarWithChildren>
+            </div>
         </div>
     );
 };
