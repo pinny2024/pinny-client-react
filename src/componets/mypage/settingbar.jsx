bbf88caf6c3bcc32cd09c9a80f619635580726ce
GoEun <w2219@e-mirim.hs.kr>
2024-06-19 22:25:33 +0900

    Mybadge 페이지 구현 and css

--- src/componets/mypage/settingbar.jsx
+++ src/componets/mypage/settingbar.jsx
@@ -12,10 +12,12 @@ const SettingBar = () => {
                     <Icon className={styles['icon']} icon="iconamoon:arrow-right-2-thin" />
                 </div>
             </Link>
+            <Link to="/mypage/mybadge">
             <div className={styles['setting-bar-attribute']}>
                 <p>내 뱃지</p>
                 <Icon className={styles['icon']} icon="iconamoon:arrow-right-2-thin" />
             </div>
+            </Link>
             <div className={styles['setting-bar-attribute']}>
                 <p>로그아웃</p>
             </div>
